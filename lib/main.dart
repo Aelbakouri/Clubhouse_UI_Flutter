39233ff3589082dde93a5902cc8568b420925e8e
Allal EL BAKOURI <95944107+Aelbakouri@users.noreply.github.com>
2021-12-17 15:59:21 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:clubhouse_ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 void main() {
   runApp(MyApp());
 }
